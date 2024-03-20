09647214dd8ea20b8c1c3c34706b9ccf68b1f9ed
c0re100 <corehusky@gmail.com>
2024-03-21 04:52:06 +0800

    Write default config

--- Config.cs
+++ Config.cs
@@ -18,20 +18,39 @@ public class TSKConfig
             var doc = JsonDocument.Parse(content);
             var config = doc.RootElement;
 
+            var needWrite = false;
+
             if (config.TryGetProperty("speed", out var sValue))
             {
                 Speed = sValue.GetDouble();
+            }
+            else
+            {
+                Speed = 0.5;
+                needWrite = true;
             }
 
             if (config.TryGetProperty("fps", out var fValue))
             {
                 FPS = fValue.GetInt32();
             }
+            else
+            {
+                FPS = 60;
+                needWrite = true;
+            }
 
             if (config.TryGetProperty("translation", out var tValue))
             {
                 TranslationEnabled = tValue.GetBoolean();
             }
+            else
+            {
+                TranslationEnabled = true;
+                needWrite = true;
+            }
+
+            if (needWrite) WriteJsonFile(Speed, FPS, TranslationEnabled);
 
             Plugin.Global.Log.LogInfo("Current setting:");
             Plugin.Global.Log.LogInfo("Game speed(each step): " + Speed);
@@ -43,6 +62,31 @@ public class TSKConfig
             Plugin.Global.Log.LogWarning("config.json not found!!!");
             Plugin.Global.Log.LogWarning("Using default config.");
             Speed = 0.5;
+            FPS = 60;
+            TranslationEnabled = true;
+
+            // Create default JSON file
+            WriteJsonFile(0.5, 60, true);
         }
+    }
+
+    public static void WriteJsonFile(double speed, int fps, bool enabled)
+    {
+        var config = new config
+        {
+            speed = speed,
+            fps = fps,
+            translation = enabled
+        };
+
+        var json = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText("./BepInEx/plugins/config.json", json);
+    }
+
+    public class config
+    {
+        public double speed { get; set; }
+        public int fps { get; set; }
+        public bool translation { get; set; }
     }
 }
